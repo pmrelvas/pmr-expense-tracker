74876554f85b6c4810b2fa1029bfe1b8d977c729
Pedro Relvas <pedrorelvas@Pedros-MacBook-Air.local>
2024-10-13 11:35:24 +0100

    feat(PET-1): Project setup

---
+++ pmr-expense-tracker-be/src/main/java/pt/pmrelvas/pmr_expense_tracker/PmrExpenseTrackerApplication.java
@@ -0,0 +1,13 @@
+package pt.pmrelvas.pmr_expense_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmrExpenseTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmrExpenseTrackerApplication.class, args);
+	}
+
+}
--- pmr-expense-tracker-be/src/native-test/java/pt/pmrelvas/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package pt.pmrelvas;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
