94512ab627bb880cffe24dd8ada673f0dff8d2ca
Pedro Relvas <pedrorelvas@MacBookAir.home>
2024-10-19 10:36:17 +0100

    feat(PET-1): added domain module with a dummy usecase

---
+++ pmr-expense-tracker-be/domain/src/main/java/pt/pmrelvas/pmr_expense_tracker/usecases/SayHelloUseCase.java
@@ -0,0 +1,11 @@
+package pt.pmrelvas.pmr_expense_tracker.usecases;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SayHelloUseCase {
+
+    public String execute() {
+        return "Hello world";
+    }
+}
