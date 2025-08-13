2b9a4e5ae73a9a4c5087cc5f2b3f341e4234ce4d
Pedro Relvas <pedromiguelrelvas@gmail.com>
2025-08-13 23:34:47 +0100

    feat(PET-3): install primeng and added navigation component

--- pmr-expense-tracker-fe/src/app/app.config.ts
+++ pmr-expense-tracker-fe/src/app/app.config.ts
@@ -1,12 +1,26 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
-
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 import { routes } from './app.routes';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+          preset: Aura,
+          options: {
+            prefix: 'p',
+            darkModeSelector: 'system',
+            cssLayer: false
+          }
+      }
+    })
   ]
 };
---
+++ pmr-expense-tracker-fe/src/app/navigation/navigation/navigation.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { Menubar } from 'primeng/menubar';
+import { BadgeModule } from 'primeng/badge';
+import { AvatarModule } from 'primeng/avatar';
+import { InputTextModule } from 'primeng/inputtext';
+import { Ripple } from 'primeng/ripple';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'pet-navigation',
+  imports: [Menubar, BadgeModule, AvatarModule, InputTextModule, Ripple, CommonModule],
+  templateUrl: './navigation.html',
+  styleUrl: './navigation.scss'
+})
+export class Navigation {
+
+  items: MenuItem[] = [
+    {
+      label: 'Home',
+    },
+    {
+      label: 'Categories',
+    },
+    {
+      label: 'Expenses',
+    },
+  ];
+}
